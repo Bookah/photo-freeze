ae721843c3ce9ca4834376399e051f39060b9396
Lukas Legters <loekas@gmail.com>
2017-12-27 10:21:02 -0500

    ES6 ecercice

---
+++ exercises/ES6-exercise.js
@@ -0,0 +1,10 @@
+// Write a function using ES5(!) that counts down from a start value until 0 (hint: setTimeout()). 
+// It should have one mandatory parameter that tells it where to start
+// And a second optional parameter telling the interval between counts (default 1000 milliseconds)
+
+//count(10)
+//count(10, 500)
+
+// Now rewrite the function using ES6
+
+// Check it at https://lebab.io/try-it
