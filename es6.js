8fe3c7c425d7e18316e8a47b4b7da38ecda58199
Lukas Legters <loekas@gmail.com>
2017-12-27 06:40:45 -0500

    fixed template

---
+++ es6.js
@@ -0,0 +1,15 @@
+// Introduction (history)
+
+// Block bindings (var, let, const, (re)declaring, block bindings in loops, functions in loops (IFEE))
+
+// Functions (default parameters(functions, objects), arrow functions(declaring, arr.sort))
+
+// Objects (shorthand property initializer, concise methods, computed property names, Object.assign (not a deep copy))
+
+// Destructuring (declaring, nested destructuring, default values, array destructuring, swapping variables)
+
+// Classes (es5(contructor + prototype), static, inheritance)
+
+// Arrays (find(), findIndex(), fill())
+
+
